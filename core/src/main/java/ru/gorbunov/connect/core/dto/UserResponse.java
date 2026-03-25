9fa88886ac53018d9ed3a30784598bc9c25291a1
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-25 14:41:15 +0700

    feat(core): add UserResponse class

---
+++ core/src/main/java/ru/gorbunov/connect/core/dto/UserResponse.java
@@ -0,0 +1,9 @@
+package ru.gorbunov.connect.core.dto;
+
+public record UserResponse(
+        Long id,
+        String userName,
+        String email,
+        String firstName,
+        String lastName
+) {}
