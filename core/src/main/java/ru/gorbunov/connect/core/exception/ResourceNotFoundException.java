99e54976947666a59325a63f969dc216a6269fcc
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-25 18:07:32 +0700

    feat(core): add ResourceNotFoundException

---
+++ core/src/main/java/ru/gorbunov/connect/core/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.gorbunov.connect.core.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
