1dd9c74414af8459fe14d569d66681bae590364b
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-25 18:32:54 +0700

    feat(core): add existByEmail, existByUserName metods in the UserRepository

--- core/src/main/java/ru/gorbunov/connect/core/repository/UserRepository.java
+++ core/src/main/java/ru/gorbunov/connect/core/repository/UserRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.gorbunov.connect.core.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existByEmail(String email);
+    Boolean existByUserName(String userName);
 }
