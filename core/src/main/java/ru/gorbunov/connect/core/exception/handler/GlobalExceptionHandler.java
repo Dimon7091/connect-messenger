105ee1949e97e5323f5d298a6de752a4f6b86c0f
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-25 18:17:39 +0700

    feat(core) add handleValidationErrors in the GlobalExceptionHandler

--- core/src/main/java/ru/gorbunov/connect/core/exception/handler/GlobalExceptionHandler.java
+++ core/src/main/java/ru/gorbunov/connect/core/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.gorbunov.connect.core.exception.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.gorbunov.connect.core.exception.ConflictException;
@@ -9,10 +10,20 @@ import ru.gorbunov.connect.core.exception.EmailAlreadyExistsException;
 import ru.gorbunov.connect.core.exception.ResourceAlreadyExistsException;
 import ru.gorbunov.connect.core.exception.ResourceNotFoundException;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.unprocessableEntity().body(errors);
+    }
 
     @ExceptionHandler
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
