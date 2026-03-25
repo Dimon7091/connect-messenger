9c7398ea53b7b02afa9d83848d5f6f4a5aa4dd69
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-25 15:06:48 +0700

    chore(core): add spring-boot-starter-validation dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
