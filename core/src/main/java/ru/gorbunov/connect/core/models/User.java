e7a62531b0d4e43cb31e4666f392eab2948dbdbc
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-25 09:55:44 +0700

    style(core): refactor User.java imports

--- core/src/main/java/ru/gorbunov/connect/core/models/User.java
+++ core/src/main/java/ru/gorbunov/connect/core/models/User.java
@@ -1,6 +1,13 @@
 package ru.gorbunov.connect.core.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
