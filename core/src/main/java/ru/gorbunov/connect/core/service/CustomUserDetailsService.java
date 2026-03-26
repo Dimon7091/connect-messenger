f8da13e1267f3226b545548e5e79d7b8519c1697
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-26 15:52:03 +0700

    feat(core): add CustomUserDetailsService

---
+++ core/src/main/java/ru/gorbunov/connect/core/service/CustomUserDetailsService.java
@@ -0,0 +1,19 @@
+package ru.gorbunov.connect.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.gorbunov.connect.core.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user with username: " + username + " not found"));
+    }
+}
