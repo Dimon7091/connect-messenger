f72816cdb88e64ed8a5110cb8c1211ae0e59db47
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-26 17:29:14 +0700

    feat(web): add JwtUtil

---
+++ web/src/main/java/ru/gorbunov/connect/web/controller/util/JwtUtil.java
@@ -0,0 +1,35 @@
+package ru.gorbunov.connect.web.controller.util;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Component;
+import ru.gorbunov.connect.core.models.User;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+@Component
+public class JwtUtil {
+    @Autowired
+    private JwtEncoder encoder;
+
+    public String generateToken(User user) {
+
+        // Текущее время в UTC
+        ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now.toInstant())
+                .expiresAt(now.plusMonths(1).toInstant())  // ✅ +1 месяц
+                .subject(user.getUserName())
+                .claim("userId", user.getId())
+                .claim("role", user.getRoles())
+                .build();
+
+        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
