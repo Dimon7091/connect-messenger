2308c97e6b371fc81b9418cbd8c6d65c682a0a6e
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-26 15:54:32 +0700

    feat(core): add findByUserName,findByEmail methods to UserRepository

--- core/src/main/java/ru/gorbunov/connect/core/models/User.java
+++ core/src/main/java/ru/gorbunov/connect/core/models/User.java
@@ -12,6 +12,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 
--- core/src/main/java/ru/gorbunov/connect/core/repository/UserRepository.java
+++ core/src/main/java/ru/gorbunov/connect/core/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package ru.gorbunov.connect.core.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import ru.gorbunov.connect.core.models.User;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
     Boolean existsByUserName(String userName);
+    Optional<UserDetails> findByUserName(String userName);
+    Optional<User> findByEmail(String email);
 }
