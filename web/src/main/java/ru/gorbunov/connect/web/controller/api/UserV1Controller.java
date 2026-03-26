e0aa9d0de9b17b4d7c7b5beeffba8fefeee36c83
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-26 14:15:52 +0700

    feat(web): add UserV1Controller, create basic CRUD operations

---
+++ web/src/main/java/ru/gorbunov/connect/web/controller/api/UserV1Controller.java
@@ -0,0 +1,83 @@
+package ru.gorbunov.connect.web.controller.api;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.gorbunov.connect.core.dto.UserCreateRequest;
+import ru.gorbunov.connect.core.dto.UserPatchUpdateRequest;
+import ru.gorbunov.connect.core.dto.UserPutUpdateRequest;
+import ru.gorbunov.connect.core.dto.UserResponse;
+import ru.gorbunov.connect.core.service.UserService;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserV1Controller {
+
+    private final UserService userService;
+
+    public UserV1Controller(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<UserResponse> create(@Valid @RequestBody UserCreateRequest requestData) {
+        var savedUser = userService.create(requestData);
+        return ResponseEntity.created(URI.create("api/v1/users/" + savedUser.id()))
+                .body(savedUser);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<UserResponse>> index() {
+        var users = userService.findAll();
+        return ResponseEntity.ok()
+                .body(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> show(@PathVariable("id") Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok()
+                .body(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> putUpdate(
+            @PathVariable("id") Long id,
+            @RequestBody UserPutUpdateRequest requestData
+    ) {
+        var updatedUser = userService.putUpdate(id, requestData);
+        return ResponseEntity.ok()
+                .body(updatedUser);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserResponse> patchUpdate(
+            @PathVariable("id") Long id,
+            @RequestBody UserPatchUpdateRequest requestData
+    ) {
+        var updatedUser = userService.patchUpdate(id, requestData);
+        return ResponseEntity.ok()
+                .body(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id) {
+        userService.delete(id);
+    }
+}
