927f1586a72922cc4304c56db2ebc0a380683bc2
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-27 09:50:24 +0700

    chore(core): add spring-boot-starter-security dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     // Spring
     implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
     // Маппер
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
