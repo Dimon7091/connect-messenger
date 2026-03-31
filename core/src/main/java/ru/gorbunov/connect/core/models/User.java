6e1a9dd13dc1dd2bf0f0f10846c26afac46549ac
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-03-31 18:03:26 +0700

    feat(core): add Builder to User entity

--- core/src/main/java/ru/gorbunov/connect/core/models/User.java
+++ core/src/main/java/ru/gorbunov/connect/core/models/User.java
@@ -14,7 +14,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -29,6 +32,9 @@ import java.util.Set;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name = "users")
