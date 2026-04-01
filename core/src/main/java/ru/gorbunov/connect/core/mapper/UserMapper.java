f59b0994706db4919b27a8f6a1b7f9578d524c3d
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-04-01 19:08:57 +0700

    feat(core): add hashPassord method to the UserMapper

--- core/src/main/java/ru/gorbunov/connect/core/mapper/UserMapper.java
+++ core/src/main/java/ru/gorbunov/connect/core/mapper/UserMapper.java
@@ -1,10 +1,14 @@
 package ru.gorbunov.connect.core.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.gorbunov.connect.core.dto.UserCreateRequest;
 import ru.gorbunov.connect.core.dto.UserPatchUpdateRequest;
 import ru.gorbunov.connect.core.dto.UserPutUpdateRequest;
@@ -18,6 +22,10 @@ import ru.gorbunov.connect.core.models.User;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class UserMapper {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Mapping(source = "password", target = "passwordHash", qualifiedByName = "hashPassword")
     public abstract User toEntity(UserCreateRequest dto);
 
     public abstract UserResponse toDto(User entity);
@@ -25,4 +33,9 @@ public abstract class UserMapper {
     public abstract void putUpdate(UserPutUpdateRequest dto, @MappingTarget User entity);
 
     public abstract void patchUpdate(UserPatchUpdateRequest dto, @MappingTarget User entity);
+
+    @Named("hashPassword")
+    protected String hashPassword(String rawPassword) {
+        return rawPassword == null ? null : passwordEncoder.encode(rawPassword);
+    }
 }
