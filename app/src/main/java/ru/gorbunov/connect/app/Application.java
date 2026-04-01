4a1072c9d105463412915d386171e6608eeac06d
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-04-01 19:30:02 +0700

    chore(app): update config in the Application.class

--- app/src/main/java/ru/gorbunov/connect/app/Application.java
+++ app/src/main/java/ru/gorbunov/connect/app/Application.java
@@ -2,8 +2,14 @@ package ru.gorbunov.connect.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.gorbunov.connect")
+@EnableJpaRepositories(basePackages = "ru.gorbunov.connect.core.repository")
+@EntityScan(basePackages = "ru.gorbunov.connect.core.models")
+@EnableJpaAuditing
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
