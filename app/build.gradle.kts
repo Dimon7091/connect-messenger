7bdb7475412e752659bdf09a12757116f85e7a8c
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-04-01 19:24:20 +0700

    chore(app): update dependencies in the app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     implementation(project(":core"))
     implementation(project(":web"))
 
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
