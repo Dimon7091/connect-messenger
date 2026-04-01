008edc3d3dafd2c0fd86b6e3455a16630b98fadd
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-04-01 13:52:19 +0700

    add findAllByRoles method to the UserRepository

--- core/src/main/java/ru/gorbunov/connect/core/repository/UserRepository.java
+++ core/src/main/java/ru/gorbunov/connect/core/repository/UserRepository.java
@@ -2,8 +2,10 @@ package ru.gorbunov.connect.core.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import ru.gorbunov.connect.core.models.Role;
 import ru.gorbunov.connect.core.models.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -11,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByUserName(String userName);
     Optional<UserDetails> findByUserName(String userName);
     Optional<User> findByEmail(String email);
+    List<User> findAllByRoles(Role role);
 }
