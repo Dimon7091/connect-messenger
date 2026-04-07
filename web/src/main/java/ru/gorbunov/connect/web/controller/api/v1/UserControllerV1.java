82ab30dcc653d275a03f321bd8b3e3af6a61fc5e
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-04-07 16:18:34 +0700

    refactor(web): refactor index method in the UserControllerV1, this return ResponseEntity<Page<UserResponse>> now

--- web/src/main/java/ru/gorbunov/connect/web/controller/api/v1/UserControllerV1.java
+++ web/src/main/java/ru/gorbunov/connect/web/controller/api/v1/UserControllerV1.java
@@ -1,6 +1,10 @@
 package ru.gorbunov.connect.web.controller.api.v1;
 
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.gorbunov.connect.core.dto.UserCreateRequest;
@@ -43,8 +48,12 @@ public class UserControllerV1 {
     }
 
     @GetMapping("")
-    public ResponseEntity<List<UserResponse>> index() {
-        var users = userService.findAll();
+    public ResponseEntity<Page<UserResponse>> index(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        var users = userService.findAll(pageable);
         return ResponseEntity.ok()
                 .body(users);
     }
