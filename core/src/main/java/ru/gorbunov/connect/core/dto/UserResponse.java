c9e71de8d940431d10586d3082a371fab60e5257
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-04-08 13:13:33 +0700

    feat(core): add roles field to UserResponse

--- core/src/main/java/ru/gorbunov/connect/core/dto/UserResponse.java
+++ core/src/main/java/ru/gorbunov/connect/core/dto/UserResponse.java
@@ -1,11 +1,14 @@
 package ru.gorbunov.connect.core.dto;
 
+import java.util.Set;
+
 public record UserResponse(
         Long id,
         String userName,
         String email,
         String firstName,
         String lastName,
+        Set<String> roles,
         String createdAt,
         String lastSeen
 ) {}
