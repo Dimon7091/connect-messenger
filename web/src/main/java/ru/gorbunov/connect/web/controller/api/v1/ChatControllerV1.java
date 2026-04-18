fde773aaaf1d33d167a03054472f03a8108e98d0
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-04-18 19:47:52 +0700

    feat(web): add ChatControllerV1

---
+++ web/src/main/java/ru/gorbunov/connect/web/controller/api/v1/ChatControllerV1.java
@@ -0,0 +1,53 @@
+package ru.gorbunov.connect.web.controller.api.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.gorbunov.connect.core.dto.chat.ChatCreateOrGetRequest;
+import ru.gorbunov.connect.core.dto.chat.ChatResponse;
+import ru.gorbunov.connect.core.mapper.ChatMapper;
+import ru.gorbunov.connect.core.service.ChatService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/chats")
+public class ChatControllerV1 {
+
+    @Autowired
+    private ChatService chatService;
+
+    @Autowired
+    private ChatMapper mapper;
+
+    @PostMapping("")
+    private ChatResponse createOrGetDirectChat(@RequestBody ChatCreateOrGetRequest requestData) {
+        var chat = chatService.createOrGetDirectChat(
+                requestData.participants().getFirst(), requestData.participants().getLast()
+        );
+        return mapper.toDto(chat);
+    }
+
+    @GetMapping("/{id}")
+    private List<ChatResponse> getAllUserChats(@PathVariable("id") Long userId) {
+        var chats = chatService.findAllDirectChats(userId);
+        return chats.stream()
+                .map(chat -> mapper.toDto(chat))
+                .toList();
+    }
+
+    @DeleteMapping("/{chatId}/participants/{participant}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteChatParticipant(
+            @PathVariable("chatId") Long chatId,
+            @PathVariable("participant") Long participantId) {
+        chatService.deleteChatForUser(chatId, participantId);
+    }
+}
