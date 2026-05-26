d64b67329c4a5d96471174ac7ec97d4cc5066b04
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-05-26 10:09:05 +0700

    feat: add a new infra module with S3StorageProvider, and config for it

---
+++ core/src/main/java/ru/gorbunov/connect/core/models/FileStorageProvider.java
@@ -0,0 +1,9 @@
+package ru.gorbunov.connect.core.models;
+
+import java.io.InputStream;
+
+public interface FileStorageProvider {
+    void upload(String key, byte[] content, String contentType, StorageType type);
+    InputStream download(String key, StorageType type);
+    String generatePresignedUrl(String key, StorageType type, long durationInMinutes);
+}
---
+++ core/src/main/java/ru/gorbunov/connect/core/models/StorageType.java
@@ -0,0 +1,8 @@
+package ru.gorbunov.connect.core.models;
+
+import java.io.InputStream;
+
+public enum StorageType {
+    AVATAR,
+    CHAT_FILE
+}
