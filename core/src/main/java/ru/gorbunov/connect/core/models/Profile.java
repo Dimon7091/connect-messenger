d616c2657e512569fc07cab3eb49cfadacc414ab
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-05-28 13:47:38 +0700

    refactor(core): delete unused imports in Profile

--- core/src/main/java/ru/gorbunov/connect/core/models/Profile.java
+++ core/src/main/java/ru/gorbunov/connect/core/models/Profile.java
@@ -1,19 +1,8 @@
 package ru.gorbunov.connect.core.models;
 
-import io.hypersistence.utils.hibernate.id.Tsid;
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
